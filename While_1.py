c070e9793df393024dda0b948d79e9b916c8b7d5
lesly500 <leslyhernandez@colegioguanenta.edu.co>
2025-02-13 18:38:09 -0500

    se crearon nuevos archivos

---
+++ While_1.py
@@ -0,0 +1,11 @@
+# input
+
+n = int(input("digite el valor de n:"))
+
+# processing
+s = 0
+i = 1
+while i<=n:
+    s=s+i
+    i=i+1
+print("La suma de los"+str(n)+"primeros numeros naturales es "+str(s))
